a4a7257b926577614d66567751581f7d9b87c682
Rakibul Islam <rakibul241221@gmail.com>
2022-09-22 16:22:54 +0600

    use bootstra style

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -3,7 +3,7 @@ import './Country.css'
 const Country = (props) => {
     const { name, area, region, population, flags } = props.country
     return (
-        <div className='country'>
+        <div className='country bg-primary'>
             <h3>{name.common}</h3>
             <img src={flags.png} alt='' />
             <p><span>Area: {area}</span> <span>Population: {population}</span></p>
